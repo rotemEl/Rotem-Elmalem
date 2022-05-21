cbbf839d503672515463dba143a05c9b8b111845
rotemEL <elmaler@bgu.ac.il>
2022-05-21 16:18:58 +0300

    desien

--- static/rotems_javaScript.js
+++ static/rotems_javaScript.js
@@ -1,19 +1,39 @@
 
 
+window.addEventListener('load', main);
+
 function ApproveSub() {
     var text = "Cool!";
     document.getElementById("button").innerHTML = text;
 }
 
+function main() {
+  const title = document.getElementById("title");
+  title.addEventListener("mouseover", changeFlower);
+  title.addEventListener("mouseout", changeDarkFlower);
 
-
-
-//nav bar
-const activePge = window.location.href;
-const navList = document.querySelectorAll('nav a').
-forEach(link => {
-    if(link.href == activePge){
-        console.log(activePge);
-        link.classList.add('active');
-    }
+   //nav bar
+  const activePage = window.location.pathname;
+  const navLinks = document.querySelectorAll('nav a').forEach(link => {    
+    if(link.href.includes(`${activePage}`)){
+    link.classList.add('active');
+  }
 });
+
+function changeFlower() {
+  title.classList.remove('dark_flower');
+  title.classList.add('flower');
+}
+
+
+function changeDarkFlower() {
+  title.classList.remove('flower');
+  title.classList.add('dark_flower');
+}
+
+
+}
+
+
+
+
